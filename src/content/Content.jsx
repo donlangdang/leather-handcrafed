07f175a4b9f2753c4aa4a9ca92ef0a43affc5f5f
donlangdang <donlangdangcc@gmail.com>
2024-03-13 23:54:44 +0700

    two bannerImage

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -1,58 +1,60 @@
-import { Box } from '@mui/material'
+import { Box, keyframes } from '@mui/material'
+
+
+const imageKeyFrame = keyframes`
+  to {
+    transform: scale(1.1)
+  }
+`
 
 function Content() {
   return (
     <Box
       sx={{
-        width: '100%',
-        height: '100vh',
+        height: 'auto',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-evenly',
-        bgcolor: 'rgb(236, 236, 236)'
+        justifyContent: 'center',
+        gap: '1rem',
+        padding: '1rem',
+        position: 'relative'
       }}
     >
       <Box
         sx={{
-          width: '40%',
-          height: '100%',
+          aspectRatio: '3/4',
           overflow: 'hidden'
         }}
       >
         <Box
+          component='img'
+          src='https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/370428802_6537437939686163_1798569505760996482_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=wYGNL44BMQoAX8VSNLS&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfChUVr5t78obbU-OPJbk6tSCKJW9l6utwOP26nd0x8lUQ&oe=65F616F3'
           sx={{
-            backgroundImage: 'url(https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/370428802_6537437939686163_1798569505760996482_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=wYGNL44BMQoAX8VSNLS&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfChUVr5t78obbU-OPJbk6tSCKJW9l6utwOP26nd0x8lUQ&oe=65F616F3)',
             width: '100%',
             height: '100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
             '&:hover': {
-              transform: 'scale(1.2)'
+              animation: `${imageKeyFrame} 0.3s ease-in-out 1 forwards`
             }
           }}
-        ></Box>
+        />
       </Box>
       <Box
         sx={{
-          width: '40%',
-          height: '100%',
+          aspectRatio: '3/4',
           overflow: 'hidden'
         }}
       >
         <Box
+          component='img'
+          src='https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/366945492_6484370364992921_7237003014560211327_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4H4nWEjJerMAX-07bjm&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfAVkcPFqe0uGn6tVNqrRe9V8fCMMP2PFEUOlq4CUOtGIg&oe=65F72424'
           sx={{
-            backgroundImage: 'url(https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/370428802_6537437939686163_1798569505760996482_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=wYGNL44BMQoAX8VSNLS&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfChUVr5t78obbU-OPJbk6tSCKJW9l6utwOP26nd0x8lUQ&oe=65F616F3)',
             width: '100%',
             height: '100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
             '&:hover': {
-              transform: 'scale(1.2)'
+              animation: `${imageKeyFrame} 0.3s ease-in-out 1 forwards`
             }
           }}
-        ></Box>
+        />
       </Box>
     </Box>
   )
