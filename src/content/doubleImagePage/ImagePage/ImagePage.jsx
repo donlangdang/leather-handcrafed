f6740ecb58fb942a3853905e74dc7e529a4c9c9f
donlangdang <donlangdangcc@gmail.com>
2024-03-14 21:16:00 +0700

    lam phan content va cbi sap xep lai foder va file

--- src/content/doubleImagePage/ImagePage/ImagePage.jsx
+++ src/content/doubleImagePage/ImagePage/ImagePage.jsx
@@ -1,3 +1,5 @@
+import Typography from '@mui/material/Typography'
+import MyButton from '~/navBar/myButtom'
 import { Box, keyframes } from '@mui/material'
 
 
@@ -11,21 +13,70 @@ function ImagePage({ imageUrl }) {
     <Box
       sx={{
         aspectRatio: '3/4',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        zIndex: 0,
+        position: 'relative'
       }}
     >
       <Box
-        component='img'
-        src={imageUrl}
-        alt="Đây là ảnh"
         sx={{
-          width: '100%',
-          height: '100%',
           '&:hover': {
             animation: `${imageKeyFrame} 0.3s ease-in-out 1 forwards`
           }
         }}
-      />
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: '20%',
+            left: '50%',
+            transform: 'translate(-50%, 0)',
+            // bgcolor: '#333',
+            zIndex: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            textTransform: 'uppercase'
+          }}
+        >
+          <Typography
+            sx={{
+              color: 'white'
+            }}
+            variant="h4" align='center'
+            gutterBottom
+          >
+            {imageUrl.title}
+          </Typography>
+          <MyButton
+            sx={{
+              fontSize: '0.75rem',
+              padding: '1rem 1.5rem',
+              bgcolor: 'white',
+              borderRadius: '3%',
+              border: '1px solid white',
+              boxSizing: 'content-box',
+              fontWeight: '700',
+              '&::after': {
+                bottom: '30%',
+                width: '70%'
+              }
+            }}
+          >
+            KHÁM PHÁ SẢN PHẨM
+          </MyButton>
+        </Box>
+        <Box
+          component='img'
+          src={imageUrl.url}
+          alt="Đây là ảnh"
+          sx={{
+            width: '100%',
+            height: '100%'
+          }}
+        />
+      </Box>
     </Box>
   )
 }
