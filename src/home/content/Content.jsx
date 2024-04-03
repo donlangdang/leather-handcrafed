6b77c5b3e08bbee544297518c167f44c9b0ce7d3
donlangdang <donlangdangcc@gmail.com>
2024-04-03 23:23:44 +0700

    first commit to aboutUs

--- src/home/content/Content.jsx
+++ src/home/content/Content.jsx
@@ -35,8 +35,8 @@ const lineTwo = [
 function Content() {
   return (
     <>
-      <DoubleImagePage imageUrl={imageUrl} Threshold='200'/>
-      <DoubleImagePage imageUrl={lineTwo} Threshold='700'/>
+      <DoubleImagePage imageUrl={imageUrl}/>
+      <DoubleImagePage imageUrl={lineTwo}/>
       <BannerCenter />
     </>
   )
