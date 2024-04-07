803cca006fc7213b6d7465db2543e15a5ac78d28
donlangdang <donlangdangcc@gmail.com>
2024-04-08 01:04:41 +0700

    title.jsx done

---
+++ src/aboutUs/title/Title.jsx
@@ -0,0 +1,59 @@
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import StraightIcon from '@mui/icons-material/Straight'
+
+function Title() {
+  return (
+    <Box
+      sx={{
+        color: '#f7d7c8',
+        width: '50%',
+        height: 'auto',
+        textAlign: 'center',
+        marginBottom: '8rem'
+      }}
+    >
+      <Typography
+        variant='h2'
+        gutterBottom
+        sx={{ fontWeight: '500' }}
+      >
+        1
+      </Typography>
+      <Divider
+        sx={{
+          bgcolor: 'white',
+          margin: '4rem 0 4rem 0'
+        }}
+      />
+      <Typography
+        variant='h2'
+        gutterBottom
+        sx={{ fontWeight: '500' }}
+      >
+        We design People Inspired Experiences that create positive change in people&apos;s lives.
+      </Typography>
+      <StraightIcon
+        color='white'
+        fontSize='large'
+        sx={{
+          fontSize: '4rem',
+          transform: 'rotate(180deg)',
+          margin: '2rem 0 2rem 0'
+        }}
+      />
+      <Typography
+        variant='h6'
+        gutterBottom
+        sx={{
+          fontWeight: '500'
+        }}
+      >
+        VIEW PORTFOLIO
+      </Typography>
+    </Box>
+  )
+}
+
+export default Title
