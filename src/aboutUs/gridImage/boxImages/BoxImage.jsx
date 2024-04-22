e6dfb2ecaec40c712bf4ffe11144a86da604a05f
donlangdang <donlangdangcc@gmail.com>
2024-04-23 00:21:57 +0700

    lam next and prev images dang co bug

--- src/aboutUs/gridImage/boxImages/BoxImage.jsx
+++ src/aboutUs/gridImage/boxImages/BoxImage.jsx
@@ -1,11 +1,14 @@
-import Dialog from '@mui/material/Dialog'
-import DialogTitle from '@mui/material/DialogTitle'
-import Box from '@mui/material/Box'
-import { useState } from 'react'
-import CloseIcon from '@mui/icons-material/Close'
-import { DialogActions, DialogContent, DialogContentText } from '@mui/material'
-import Slide from '@mui/material/Slide'
 import * as React from 'react'
+import { useState } from 'react'
+import Dialog from '@mui/material/Dialog'
+import CloseIcon from '@mui/icons-material/Close'
+import Slide from '@mui/material/Slide'
+import imagesGridAboutUs from '~/apis/imagesGridAboutUs'
+import IconButton from '@mui/material/IconButton'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import { keyframes } from '@mui/material'
+import Box from '@mui/material/Box'
 
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -13,7 +16,29 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 })
 
 
-function BoxImage({ gridImage }) {
+const nextAnimationImages = keyframes`
+  from {
+    opacity: 1;
+    transform: translate(100%, 0)
+  }
+  to {
+    opacity: 1;
+    transform: translate(0, 0)
+  }
+`
+
+const prevAnimationImages = keyframes`
+  from {
+    opacity: 1;
+    transform: translate(-100%, 0)
+  }
+  to {
+    opacity: 1;
+    transform: translate(0, 0)
+  }
+`
+
+function BoxImage({ index }) {
 
   const [open, setOpen] = useState(false)
   const handleOpen = () => {
@@ -21,6 +46,14 @@ function BoxImage({ gridImage }) {
   }
   const handleClose = () => {
     setOpen(false)
+  }
+
+  const [nextImageIndex, setNextImageIndex] = useState(index)
+  const handleNextImage = () => {
+    setNextImageIndex((nextImageIndex + 1) % imagesGridAboutUs.length)
+  }
+  const handlePrevImage = () => {
+    setNextImageIndex((nextImageIndex - 1 + imagesGridAboutUs.length) % imagesGridAboutUs.length)
   }
 
   return (
@@ -44,12 +77,11 @@ function BoxImage({ gridImage }) {
           objectPosition: 'center',
           aspectRatio: '1/1',
           transition: 'transform 0.5s',
-          filter: 'grayscale(100%)',
           '&:hover': {
             transform: 'scale(1.1)'
           }
         }}
-        src={gridImage.url}
+        src={imagesGridAboutUs[index].url}
       />
       <Dialog
         TransitionComponent={Transition}
@@ -70,8 +102,8 @@ function BoxImage({ gridImage }) {
         >
           <CloseIcon
             sx={{
-              height: '3rem',
-              width: '3rem',
+              height: '2.5rem',
+              width: '2.5rem',
               cursor: 'pointer',
               position: 'relative',
               right: '-17.5%',
@@ -82,16 +114,33 @@ function BoxImage({ gridImage }) {
             onClick={handleClose}
           />
           <Box
-            component='img'
-            src={gridImage.url}
             sx={{
-              height:'auto',
-              width: '35%',
-              objectFit: 'contain',
-              objectPosition: 'center',
-              aspectRatio: '3/4',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
             }}
-          />
+          >
+            <IconButton onClick={handlePrevImage}>
+              <ChevronLeftIcon fontSize='large' sx={{ color: 'white', height: '2.5rem', width: '2.5rem' }}/>
+            </IconButton>
+            <Box
+              component='img'
+              src={imagesGridAboutUs[nextImageIndex].url}
+              sx={{
+                height:'auto',
+                width: '35%',
+                objectFit: 'contain',
+                objectPosition: 'center',
+                aspectRatio: '3/4',
+                ...(handlePrevImage && { animation: `${prevAnimationImages} 0.5s ease-in-out 1 forwards` }),
+                ...(handleNextImage && { animation: `${nextAnimationImages} 0.5s ease-in-out 1 forwards` }),
+                // animation: `${nextAnimationImages} 0.5s ease-in-out 1 forwards`
+              }}
+            />
+            <IconButton onClick={handleNextImage}>
+              <ChevronRightIcon fontSize='large' sx={{ color: 'white', height: '2.5rem', width: '2.5rem' }}/>
+            </IconButton>
+          </Box>
         </Box>
       </Dialog>
     </Box>
